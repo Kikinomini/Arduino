b4bb37396fca60ef71d5a98f863b96ff1bd96d32
MissKunterbunt <sarah.willibald@web.de>
2017-03-14 15:54:03 +0100

    Meilenstein! Er hat das Internet gefunden! :D

---
+++ WifiTested/WifiTested.ino
@@ -0,0 +1,43 @@
+/*
+  Test for Wifi connection and ping-time
+  Change SSID and PASSWORD.
+*/
+
+#include <NanoESP.h>
+#include <SoftwareSerial.h>
+
+//Change SSID and PASSWORD here:
+
+
+
+NanoESP nanoesp = NanoESP();
+
+void setup() {
+  Serial.begin(19200);
+  nanoesp.init();
+
+  //Station Mode, Connect to WiFi
+  if (!nanoesp.configWifiStation("HuaweiP9", "testtest")) {
+    Serial.println(F("Error: WLAN not Connected\n"));
+  }
+  else {
+    Serial.println(F("WLAN Connected\n"));
+  }
+
+ //Print IP in Terminal
+  Serial.println(nanoesp.getIp());
+
+  //Time for Ping
+  int vTime = nanoesp.ping("www.google.de");
+
+  if (vTime > 0 ){
+    Serial.println("Internet Connection OK\nPing: " + String(vTime) + "ms"); 
+  }
+  else {
+    Serial.println(F("Error: Internet Connection"));
+  }
+}
+
+void loop() {
+
+}
